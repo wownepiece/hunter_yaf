51c24a5a6908a57760d9760bb472c50438a912a8
wownepiece <hunter_wyg@hotmail.com>
2016-03-08 16:01:47 +0800

    mv to www
    mv to www

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('APPLICATION_PATH', dirname(__FILE__));
-
-$application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini", 'develop');
-
-$application->bootstrap()->run();
